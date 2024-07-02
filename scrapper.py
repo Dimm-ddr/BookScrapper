07f27fcb8e6cbb8609423a5132627708e2028374
dimmddr <dmitrii.repin@pm.me>
2024-07-02 20:14:45 +0300

    fix: :bug: Fix tags and authors extraction

--- scrapper.py
+++ scrapper.py
@@ -1,11 +1,9 @@
 import os
 import re
 import sys
-
 import requests
 from bs4 import BeautifulSoup
 import json
-
 
 def scrape_goodreads(goodreads_url):
     """Scrapes book data from a Goodreads book page, including tags."""
@@ -13,59 +11,39 @@ def scrape_goodreads(goodreads_url):
     soup = BeautifulSoup(response.text, 'html.parser')
 
     print(f"Collecting data from the {goodreads_url}")
-    title = soup.select_one('.Text__title1').get_text()
+    title = soup.select_one('[data-testid="bookTitle"]').get_text(strip=True)
+    
+    # Use a set to avoid duplicates
+    authors_set = set()
+    for contributor in soup.select('span[data-testid="name"]'):
+        authors_set.add(contributor.get_text(strip=True))
+    authors = list(authors_set)
 
-    author = soup.select_one('.ContributorLink__name').text.strip()
-    cover_image = soup.select_one('.BookPage__leftColumn .BookCover__image img')['src']
-    description = soup.select_one(
-        '.BookPageMetadataSection__description .DetailsLayoutRightParagraph__widthConstrained span.Formatted').get_text(
-        strip=True)
+    cover_image = soup.select_one('div.BookCover__image img.ResponsiveImage')['src']
+    description = soup.select_one('[data-testid="description"]').get_text(strip=True)
 
     # Find the section containing the tags/genres
-    genres = soup.find_all('a', class_='Button Button--tag-inline Button--small')
-    tags = [genre.text.strip() for genre in genres if '/genres/' in genre.get('href', '')]
+    genres_list = soup.select_one('[data-testid="genresList"]')
+    genres = genres_list.select('a.Button--tag-inline span.Button__labelItem') if genres_list else []
+    tags = [genre.get_text(strip=True) for genre in genres]
 
     pages_element = soup.find('p', attrs={'data-testid': 'pagesFormat'})
-
     if pages_element:
-        # Extract the text from the element
         pages_text = pages_element.get_text(strip=True)
-
-        # Use regular expression to find all numbers in the text
         numbers = re.findall(r'\d+', pages_text)
-
-        # Assuming there's only one number in the text, which is the number of pages
-        if numbers:
-            number_of_pages = int(numbers[0])
-        else:
-            number_of_pages = None
+        number_of_pages = int(numbers[0]) if numbers else None
     else:
         number_of_pages = None
 
     return {
         'title': title,
-        'author': author,
+        'authors': authors,
         'cover_image': cover_image,
         'description': description,
         'tags': tags,
         'number_of_pages': number_of_pages,
         'link': goodreads_url,
     }
-
-
-def scrape_wikipedia(wikipedia_url):
-    """Scrapes data from a Wikipedia page."""
-    response = requests.get(wikipedia_url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    title = soup.find('h1', id='firstHeading').text.strip()
-    summary = soup.find('div', class_='mw-parser-output').p.text.strip()
-
-    return {
-        'title': title,
-        'summary': summary
-    }
-
 
 def save_data_to_json(data, filename='data.json'):
     """Saves scraped data to a JSON file."""
@@ -74,7 +52,6 @@ def save_data_to_json(data, filename='data.json'):
         return
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
-
 
 def scrape_url(url):
     if "goodreads.com" in url:
@@ -85,28 +62,21 @@ def scrape_url(url):
               "at https://github.com/Dimm-ddr/BookScrapper")
         sys.exit()
 
-
 def scrape_txt_file(input_file):
     with open(input_file, 'r') as file:
         for line in file:
-            # Strip newline and whitespace characters from the end of the line
             url = line.strip()
-            # Apply the scrape_url method to each URL
             data = scrape_url(url)
             save_to_json_file(data)
 
-
 def save_to_json_file(data):
     if data:
-        # Replace characters in title that are invalid for filenames
-        safe_title = "".join(
-            [c for c in data['title'] if c.isalpha() or c.isdigit() or c in " _-"]).rstrip()
+        safe_title = "".join([c for c in data['title'] if c.isalpha() or c.isdigit() or c in " _-"]).rstrip()
         filename = f"{safe_title}.json"
         save_data_to_json(data, filename)
         print(f"Data saved to {filename}")
     else:
         print("Failed to scrape data.")
-
 
 if __name__ == "__main__":
     argument = sys.argv[1]
@@ -117,11 +87,10 @@ if __name__ == "__main__":
     elif ".txt" in argument:
         input_file = argument
     else:
-        print("Incorrect script parameter. "
-              "Pass either goodreads URL or text file with goodreads urls")
+        print("Incorrect script parameter. Pass either goodreads URL or text file with goodreads urls")
         sys.exit()
-    data = None
 
+    data = None
     if url is not None:
         print(f"Scrapping single URL")
         data = scrape_url(url)
